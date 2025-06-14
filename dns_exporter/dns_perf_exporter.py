1ed144e249ac624d64540824477c627fc62685d3
Clément Nardi <clementnardi@gmail.com>
2025-06-14 12:41:39 +0200

    change router IP

--- dns_exporter/dns_perf_exporter.py
+++ dns_exporter/dns_perf_exporter.py
@@ -8,12 +8,12 @@ import os
 buckets = (.01, .02, .04, .07, .1, .2, .4, .7, 1.0, 2.0, float('inf'))
 
 # Create a metric to track time spent and requests made.
-freebox_time = Histogram('dns_lookup_freebox_seconds', 'Time spent running dig @192.168.1.254 google.fr', buckets=buckets)
+freebox_time = Histogram('dns_lookup_freebox_seconds', 'Time spent running dig @10.42.16.1 google.fr', buckets=buckets)
 
 # Decorate function with metric.
 @freebox_time.time()
 def dig_freebox():
-    os.system('dig +retry=0 +timeout=1 @192.168.1.254 google.fr')
+    os.system('dig +retry=0 +timeout=1 @10.42.16.1 google.fr')
     
 # Create a metric to track time spent and requests made.
 google_time = Histogram('dns_lookup_google_seconds', 'Time spent running dig @8.8.8.8 google.fr', buckets=buckets)
